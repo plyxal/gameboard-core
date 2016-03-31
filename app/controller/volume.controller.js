28bbdbd089cb469b91d63be3671173ab3df022f9
Paul C. Smith IV <paul.smith.iv@ninthavenuemedia.com>
2016-03-30 23:14:10 -0500

    integrated with device audio, need to figure out why ding isn't playing.

---
+++ app/controller/volume.controller.js
@@ -0,0 +1,56 @@
+/**
+ * Created by psmit on 2/23/2016.
+ */
+exports = module.exports = init;
+
+function init(config) {
+    'use strict';
+    var restify = inject('restify');
+    var volumeService = inject('volumeService');
+
+    /**
+     * @private
+     * @param req
+     * @param res
+     * @param next
+     */
+    var getVolume = function (req, res, next) {
+        var success = function(result) {
+            res.send({value: result});
+        };
+
+        var fail = function(error) {
+            res.send({error: 'error'});
+        };
+
+        volumeService.getVolume()
+            .then(success, fail);
+    };
+
+    /**
+     * @private
+     * @param req
+     * @param res
+     * @param next
+     */
+    var putVolume = function (req, res, next) {
+        var success = function(result) {
+            res.send({value: result});
+        };
+
+        var fail = function(error) {
+            res.send({error: 'error'});
+        };
+
+        volumeService.putVolume(req.params.value)
+            .then(success, fail);
+    };
+
+    /**
+     * @constructor
+     */
+    (function(){
+        restify.get('/volume', getVolume);
+        restify.put('/volume/:value', putVolume);
+    }())
+}
---
+++ app/service/volume.service.js
@@ -0,0 +1,63 @@
+/**
+ * Created by psmit on 2/23/2016.
+ */
+module = module.exports = init;
+
+function init(config) {
+    'use strict';
+
+    var Q = require('q');
+
+    //stub loudness so it can be disabled
+    //var loudness = {
+    //    getVolume: function() {},
+    //    setVolume: function() {}
+    //};
+
+    //if enabled, load loudness module
+    if(config.enabled)
+        var loudness = require('loudness');
+
+    /**
+     *
+     * @param value
+     */
+    var getVolume = function() {
+        var deferred = Q.defer();
+
+        loudness.getVolume(function (err, vol) {
+            console.log('loudness.getVolume: ' + err + ' - ' + vol);
+            deferred.resolve(vol);
+        });
+
+        //if not enabled, mock result
+        if(!config.enabled)
+            deferred.resolve({value: 50});
+
+
+        return deferred.promise;
+    };
+
+    /**
+     *
+     * @param value
+     */
+    var putVolume = function(value) {
+        var deferred = Q.defer();
+
+        loudness.setVolume(value, function (err) {
+            deferred.resolve(value)
+        });
+
+        //if not enabled, mock result
+        if(!config.enabled)
+            deferred.resolve({value: value});
+
+        return deferred.promise;
+    };
+
+    return {
+        getVolume: getVolume,
+        putVolume: putVolume
+    }
+}
