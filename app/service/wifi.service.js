04c1c565dc92a6c6f84a53ca372b834ae79087ab
psmithiv <paul.smith.iv@ninthavenuemedia.com>
2016-08-05 18:06:48 -0500

    continued work on wifi connectivity

--- app/service/wifi.service.js
+++ app/service/wifi.service.js
@@ -6,8 +6,17 @@ exports = module.exports = init;
 function init(config) {
     'use strict';
 
-    // var Wireless = require('wireless');
     var Q = require('q');
+
+    var networkManager = require('vendor/node-networkmanager-master/index');
+
+    networkManager.connect(function (error, networkmanager) {
+        console.log('networkmanager.NetworkManager: ', networkmanager.NetworkManager);
+        
+        networkmanager.NetworkManager.GetVersion(function (error, Version) {
+            console.log("NetworkManager Version: " + Version);
+        });
+    });
 
     /**
      * 
@@ -16,33 +25,11 @@ function init(config) {
     var getNetworks = function () {
         var deferred = Q.defer();
 
-        // var wireless = new Wireless({
-        //     iface: 'wlx00c316a0c9bb',
-        //     updateFrequency: 10, // Optional, seconds to scan for networks
-        //     connectionSpyFrequency: 2, // Optional, seconds to scan if connected
-        //     vanishThreshold: 2 // Optional, how many scans before network considered gone
+        // networkManager.connect(function (error, networkmanager) {
+        //     networkmanager.NetworkManager.GetVersion(function (error, Version) {
+        //         console.log("NetworkManager Version: " + Version);
+        //     });
         // });
-        //
-        // wireless.enable(function(err) {
-        //     console.log('enable: ', err);
-        //     wireless.start();
-        // });
-        //
-        // wireless.on('vanish', function(network) {
-        //     console.log("[  VANISH] " + network.ssid + " [" + network.address + "] ");
-        // });
-        //
-        // // A wireless network changed something about itself
-        // wireless.on('change', function(network) {
-        //     console.log("[  CHANGE] " + network.ssid);
-        // });
-        //
-        // wireless.on('signal', function(network) {
-        //     console.log("[  SIGNAL] " + network.ssid);
-        // });
-
-        // console.log('wireless.list(): ', wireless.list());
-        // deferred.resolve(wireless.list());
 
         deferred.resolve({});
 
